9fc999a95a67b9ad7ff567fd43ab2cc618afbaf9
Alec Schule <aschule@usc.edu>
2019-09-20 14:41:26 -0700

    use wordpress charset on front page

--- front-page.php
+++ front-page.php
@@ -9,6 +9,7 @@ for ($i = 0; $i < nFeaturedPages; $i++) {
 
 <html>
   <head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>" />
     <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1" />
     <link href="<?php echo get_template_directory_uri() . '/assets/css/front-page.css';?>" rel="stylesheet">
     <!-- Google Open Sans -->
