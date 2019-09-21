93a39f679f7a7b85c02f662d989d28c302346bf3
Alec Schule <aschule@usc.edu>
2019-09-20 17:49:21 -0700

    remove background from header

--- header.php
+++ header.php
@@ -18,7 +18,7 @@
 
 <body <?php body_class(); ?>>
   <!-- header -->
-  <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+  <nav class="navbar navbar-expand-md navbar-dark">
     <a class="navbar-brand" href="<?php echo get_home_url(); ?>"><?php echo get_bloginfo('name'); ?></a>
     <?php wp_nav_menu(
       array(
