ba6efcf20a4891581bc7aee9c92ff8fceb4e325c
Alec Schule <aschule@usc.edu>
2019-09-23 18:48:36 -0700

    fix navbar display for mobile devices

--- header.php
+++ header.php
@@ -20,6 +20,9 @@
   <!-- header -->
   <nav class="navbar navbar-expand-md navbar-dark">
     <a class="navbar-brand" href="<?php echo get_home_url(); ?>"><?php echo get_bloginfo('name'); ?></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar_collapse" aria-controls="navbar_collapse" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+    </button>
     <?php wp_nav_menu(
       array(
         'container'       => 'div',
